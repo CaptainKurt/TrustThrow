41259bca5f0ecdb0ebad418b9a95ae969841eab0
CaptainKurt <walkkg12@wfu.edu>
2015-10-16 17:42:40 -0400

    init commit

---
+++ TrustThrow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrustThrow
+//
+//  Created by Kurt Walker on 10/16/15.
+//  Copyright © 2015 FreebirdTechnologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
